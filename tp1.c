0d35ef2e10f6c846cf879816708946a816e4f681
NathanTeboul <44207234+NathanTeboul@users.noreply.github.com>
2018-10-23 18:21:05 +0200

    last

--- tp1.c
+++ tp1.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct s 
+typedef struct bloc
 {
 	//si 0 alors valid, sinon non valid
     int valid;
@@ -16,11 +16,11 @@ typedef struct s
     double numbloc;
     double tag; 
 }
-bloc;
+t_bloc;
 
-typedef bloc Cache [nbe][assoc];
+//typedef struct t_bloc Cache;
 
-void init_tab (int nbe, int assoc, Cache cache)
+void init_tab (int nbe, int assoc, t_bloc *cache[nbe][assoc])
 {
     int i, j;
     for (i=0; i<nbe; i++)
@@ -32,12 +32,14 @@ void init_tab (int nbe, int assoc, Cache cache)
             //cache[i][j].index=0;
             //cache[i][j].numbloc=0;
             //cache[i][j].tag=0;;
+
         }
     }
 }
 
+/*
 void check(int arg1, int arg2, int arg3, char fichier){
-	if(arg1 == Null){
+	if(arg1 == NULL){
 		printf("taille du cache non rentrée");
 		exit(0);
 	}
@@ -47,7 +49,7 @@ void check(int arg1, int arg2, int arg3, char fichier){
 			exit(0);
 		}
 	}
-	if(arg2 == Null){
+	if(arg2 == NULL){
 		printf("taille d'un bloc non rentrée");
 		exit(0);
 	}else{
@@ -56,7 +58,7 @@ void check(int arg1, int arg2, int arg3, char fichier){
 			exit(0);
 		}
 	}
-	if(arg3 == Null){
+	if(arg3 == NULL){
 		printf("degré associativité non rentrée");
 		exit(0);
 	}else{
@@ -65,7 +67,7 @@ void check(int arg1, int arg2, int arg3, char fichier){
 			exit(0);
 		}
 	}
-	if(fichier == Null){
+	if(fichier == NULL){
 		printf("fichier vide");
 		exit(0);
 	}
@@ -105,7 +107,8 @@ void affichage(Cache cache,int nbr_r, int nbr_w ){
     printf("il y a eu : %d lecture \n il y a eu : %d ecriture \n", nbr_r, nbr_w);
     choix_affichage(cache);
 }
-
+*/
+/*
 void traitement (FILE *tr, Cache cache,int Cs, int Bs, int Assoc, int Nbe){
 	int cs = Cs;
 	int bs = Bs;
@@ -123,7 +126,7 @@ void traitement (FILE *tr, Cache cache,int Cs, int Bs, int Assoc, int Nbe){
 
     while (!feof(tr))
     {
-        fscanf (f, "%c %X\n", &car, &adr);
+        fscanf (tr, "%c %X\n", &car, &adr);
         // calcul du nombre de bloc
         numbloc = adr / bs;
         //calcul de l'index (numero de la ligne du tableau Cache)
@@ -158,64 +161,98 @@ void traitement (FILE *tr, Cache cache,int Cs, int Bs, int Assoc, int Nbe){
             }
             else {
                 // methode FIFO
+                //aide
+                 fgets(fichier,"%s",type[i].nom);
+           	 	fscanf(fichier,"   %d %d",&type[i].prix,type[i].temps);
             }
         }
     }
 
     //return cache;
 }
-
-void test(){
+*/
+void test(FILE *tr){
 	// lecture du fichier
 	char car;
-	char adr;
+	char adr[10];
 
-        
-    fscanf (f, "%c %X\n", &car, &adr);
+       while (!feof(tr))
+    {  
+    fscanf (tr, "%c %s \n", &car, &adr[0]);
 
-	printf("%c %X \n", car,adr);
+	printf("%c %s \n", car, adr);
 }
-int main (char fichier){
-/*
-int main(int arg1, int arg2, int arg3, char fichier){
+}
+int main (int argc,char **argv){
+
+	//argv[0]= nomfile
+	//argv[1]= cs
+	//argv[2]= bs
+	//argv[3]= assoc
+	//argv[4]= filepara
+	int arg1 = atoi(argv[1]);
+	int arg2 = atoi(argv[2]);
+	int arg3 = atoi(argv[3]);
+
+	printf("%d %d %d \n", arg1,arg2, arg3);
+
 
 	//verif si donnees rentree en para
-	check(arg1, arg2, arg3, fichier);
+	//check(arg1, arg2, arg3, argv[4]);
 
 	int k =1024;			// taille d un octet
 	int cs = arg1*k;		// taille totale de la cache (octet)
 	int bs = arg2;			// taille d un bloc en octet
 	int assoc = arg3;		// degre associativite
-	int nbe cs/(bs*assoc);	// nombre d ensemble	
-	FILE* tr= Null;			//creation d une variable file
+	int nbe = cs/(bs*assoc);	// nombre d ensemble	
+	FILE *tr= NULL;			//creation d une variable file
 
 	//print simple pour verif.
 	printf("cache size : %d \nBloc size : %d \nAssociativite : %d \nNombre ensembles : %d\n", cs, bs, assoc, nbe);
 
     //creation du tableau correspondant au donne rentre et init a 0 de celui ci
-    Cache cache;
-    init_tab(nbe, assoc, cache);
+    t_bloc cache[nbe][assoc];
 
+    init_tab(nbe, assoc, cache[nbe][assoc]);
+/*
+int i, j;
+    for (i=0; i<nbe; i++)
+    {
+        for (j=0; j<assoc; j++)
+        {
+            cache[i][j].valid=0;
+           // Pas besoin de les init je crois
+            //cache[i][j].index=0;
+            //cache[i][j].numbloc=0;
+            //cache[i][j].tag=0;;
+
+        }
+    }
+
+      for (i=0; i<nbe; i++)
+    {
+        for (j=0; j<assoc; j++)
+        {
+    		printf("%d \n",cache[i][j].valid);
+    	}
+    }
+*/
     //compteur de read et write
     int nbr_r=0, nbr_w=0; 
 
     //ouverture du fichier passer en para avec l arg lecture seule
-	tr = fopen(fichier, "r");
+	tr = fopen(argv[4], "r");
 
 	//traitement du fichier
-	traitement(tr, cache,cs, bs, assoc, nbe);
+//	traitement(tr, cache,cs, bs, assoc, nbe);
 
 	//affichage
-	affichage(cache, nbr_r, nbr_w);
-*/
-	FILE* tr= Null;	
+//	affichage(cache, nbr_r, nbr_w);	
 
-	tr = fopen(fichier, "r");
-
-	test(fichier);
+	test(tr);
 	
 	//fermeture fichier
-	fclose(tr), tr=Null;
+	fclose(tr), tr=NULL;
 
-    return 1;
+    return 999999;
 }
